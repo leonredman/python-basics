6988adc8852f6161c5df95aba9676ef18674e82f
leonredman <leonproart@yahoo.com>
2022-06-28 08:57:36 -0700

    init commit

---
+++ python-basics-01.py
@@ -0,0 +1,9 @@
+# python basics
+
+wage = 20   # Create a variable 'wage' with value 20
+hours = 40  # Create a variable 'hours' with value 40
+weeks = 50  # Create a variable 'weeks' with value 50
+
+salary = wage * hours * weeks  # Compute salary
+print('Salary is:', end=' ')   # Print "Salary is:" on the screen
+print(salary)                  # Print salary on the screen
