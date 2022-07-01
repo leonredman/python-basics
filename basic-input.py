d581bf5cb95fba6ac7a812830e467ce9dd761319
leonredman <leonproart@yahoo.com>
2022-06-30 23:18:41 -0700

    init commit

---
+++ basic-input.py
@@ -0,0 +1,11 @@
+# basic input
+
+dog_years = int(input('Enter age of dog (in years):\n'))
+human_years = 7 * dog_years
+
+print(dog_years, 'dog years is about', end=' ')
+print (human_years, 'human years.')
+
+# yeild:
+#  Enter age of dog (in years):
+#  7 dog years is about 49 human years.
