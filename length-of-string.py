711cf23bd9dff88b8f854ca6aa413b4213ece5ab
leonredman <leonproart@yahoo.com>
2022-07-04 22:05:53 -0700

    init commit

---
+++ length-of-string.py
@@ -0,0 +1,12 @@
+# using len() to get length of a string
+
+george_v = "His Majesty George V, by the Grace of God, " \
+           "of the United Kingdom of Great Britain and " \
+           "Ireland and of the British Dominions beyond " \
+           "the Seas, King, Defender of the Faith, Emperor of India"
+gandhi = 'Mohandas Karamchand Gandhi'
+john_f_kennedy = 'JFK'
+
+print(len(george_v), 'characters is much too long of a name!')
+print(len(gandhi), 'characters is better...')
+print(len(john_f_kennedy), 'characters is short enough.')
