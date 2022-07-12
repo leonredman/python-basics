8a4476b018c1ac2314368f1cca1b5f4e4a4c95db
leonredman <leonproart@yahoo.com>
2022-07-12 00:01:41 -0700

    init commit

---
+++ for-loops.py
@@ -0,0 +1,35 @@
+# for loops
+# example 1
+
+daily_revenues = [
+    2356.23,  # Monday
+    1800.12,  # Tuesday
+    1792.50,  # Wednesday
+    2058.10,  # Thursday
+    1988.00,  # Friday
+    2002.99,  # Saturday
+    1890.75   # Sunday
+]
+
+total = 0
+for day in daily_revenues:
+    total += day
+
+average = total / len(daily_revenues)
+
+print('Weekly revenue: $%f' % total)
+print('Daily average revenue: $%f' % average)
+
+
+# example 2 
+
+channels = {
+    'MTV': 35,
+    'CNN': 28,
+    'FOX': 11,
+    'NBC': 4,
+    'CBS': 12
+}
+
+for c in channels:
+    print('%s is on channel %d' % (c, channels[c]))
