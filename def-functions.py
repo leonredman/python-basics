a1b1f4f4497cef986ee87c87c674f7cde5388c37
leonredman <leonproart@yahoo.com>
2022-07-14 23:24:06 -0700

    init commit

---
+++ def-functions.py
@@ -0,0 +1,18 @@
+# def functions
+
+def print_face():
+    face_char = 'o'
+    print('  ', face_char, ' ', face_char)  # Print eyes
+    print('    ', face_char)                # Print nose
+    print('  ', face_char*5)                # Print mouth
+    return
+
+
+print_face()
+
+''' yeild:
+o   o
+  o
+ooooo
+
+'''
