569ec0173b66644ecdb3a3a9d9105d1bbb4b1fd6
leonredman <leonproart@yahoo.com>
2022-07-17 20:19:10 -0700

    init commit

---
+++ temp-convert-celc-Farht.py
@@ -0,0 +1,22 @@
+# temperature conversion celcius to Farenheight
+
+'''Complete the program by writing and calling a 
+function that converts a temperature 
+from Celsius into Fahrenheit. Use the formula 
+F = C x 9/5 + 32. Test your program knowing that 
+50 Celsius is 122 Fahrenheit.
+'''
+
+def c_to_f():
+    pass  # Does nothing. Placeholder for function code.
+    # FIXME: Finish
+
+temp_c = float(input('Enter temperature in Celsius: '))
+temp_f = None
+
+# FIXME: Call conversion function
+temp_f = temp_c * 9/5 + 32
+
+# FIXME: Print result
+print('Fahrenheit:' , temp_f)
+ 
