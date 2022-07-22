c760cede8dc65747768dc55606a5d5eb5091edb0
leonredman <leonproart@yahoo.com>
2022-07-22 02:49:38 -0700

    init commit

---
+++ max-val-function-call.py
@@ -0,0 +1,33 @@
+# max val
+# function - call expression
+
+def find_max(num_1, num_2):
+   max_val = 0.0
+
+   if (num_1 > num_2):  # if num1 is greater than num2,
+      max_val = num_1   # then num1 is the maxVal.
+   else:                # Otherwise,
+      max_val = num_2   # num2 is the maxVal
+   return max_val
+
+num_a = 5.0
+num_b = 10.0
+num_y = 3.0
+num_z = 7.0
+max_sum = 0.0
+
+#Assign max_sum with the max of (num_a, num_b) PLUS the max of (num_y, num_z). 
+max_sum = max(num_a,num_b) + max(num_y, num_z)
+print('max_sum is:', max_sum)
+
+'''
+so it would be really cool to get a new job, to get more money, to get to work with the front end team
+to be working with the CMS team and learn what I need to learn
+To get a Degree and get good at DS Algos and then pass the interview and then get to work with a team and learn code
+that would be what I want to do
+Just to progress
+Just write DS algo in python and then I can work on tickets and get better at coding
+
+They hire me on the team and then teach me site core. it would be real cool to then start bug chasing etc.
+
+'''
