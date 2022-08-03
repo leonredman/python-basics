d476863eb6a74eaeee7ead6714912862506a0328
leonredman <leonproart@yahoo.com>
2022-08-02 21:50:25 -0700

    init commit

---
+++ Sorting-Algorithms/class-definition.py
@@ -0,0 +1,19 @@
+# Class definition
+class Node:
+    def __init__(self, initial_data):
+        self.data = initial_data
+        self.next = None
+        
+# Node initialization
+node_a = Node(95)  # Creates a node with a data value of 95
+
+#Singly linked list class defination and linked list initialization
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = No
+
+# Linked List initialization
+
+Num_list = LinkedList()  # Creates an empty linked list
